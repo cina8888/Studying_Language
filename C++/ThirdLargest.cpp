94a13d14d9ad0439e421c5c7b40fee09e92e34d3
cina8888 <lnlgamers9@gmail.com>
2026-06-03 18:39:17 +0700

    update ThirdLargest

--- C++/ThirdLargest.cpp
+++ C++/ThirdLargest.cpp
@@ -1,2 +1,11 @@
 // C++ program to find the third largest
 // element in an array.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	vector<int> arr = {3 , 1 , 2 , 6, 8};
+	return 0;
+}
