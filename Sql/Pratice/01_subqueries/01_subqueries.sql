1aba59c9cb22acf0f262b22dddd4f78461a90e5e
cina8888 <lnlgamers9@gmail.com>
2026-06-20 18:44:13 +0700

    update subqueries practice

--- Sql/Pratice/01_subqueries/01_subqueries.sql
+++ Sql/Pratice/01_subqueries/01_subqueries.sql
@@ -96,55 +96,121 @@ Display employee names
 whose salary is greater than or equal to
 the highest salary in HR department.
 */
+select emp_name
+from Employees
+where salary >=
+(select max(salary) 
+from Employees
+where dept_id = (select dept_id from Departments where dept_name = 'HR'));
 
 /*Display employee names
 whose salary is greater than or equal to
 the average salary in Finance department.
 */
+select emp_name
+from Employees
+where salary >=
+(select avg(salary) 
+from Employees
+where dept_id = (select dept_id from Departments where dept_name = 'Finance'));
 
 /* 
 Display employee names
 whose salary is less than
 the highest salary in the Marketing department.
 */
+select emp_name
+from Employees
+where salary <
+(select max(salary) 
+from Employees
+where dept_id = (select dept_id from Departments where dept_name = 'Marketing'));
 
 /* Display employee names
 whose salary is equal to
 the minimum salary in the HR department.
 */
+select emp_name
+from Employees
+where salary =
+(select min(salary) 
+from Employees
+where dept_id = (select dept_id from Departments where dept_name = 'HR'));
 
 /*
 Display employee names
 whose salary is NOT equal to
 the highest salary in the IT department.
 */
+select emp_name
+from Employees
+where salary !=
+(select max(salary) 
+from Employees
+where dept_id = (select dept_id from Departments where dept_name = 'IT'));
 
 /* Display employee names
 whose salary is not equal to
 the minimum salary in the Finance department.
 */
+select emp_name
+from Employees
+where salary !=
+(select min(salary) 
+from Employees
+where dept_id = (select dept_id from Departments where dept_name = 'Finance'));
 
 /* Display employee names
 whose salary is equal to
-the maximum salary in the department where David works.
+the maximum salary in the department where Karl works.
 */
+select emp_name
+from Employees
+where salary = 
+(select max(salary) 
+from Employees 
+where dept_id = 
+(select dept_id 
+ from Employees 
+ where emp_name = 'Karl'));
 
 /*
 Display department names
-where the average salary is less than Grace’s salary.
+where the average salary is less than Sam’s salary.
 */
+select dept_name
+from Departments
+where dept_id in 
+(select dept_id 
+from Employees 
+Group by dept_id
+having avg(salary) < (select salary from Employees where emp_name = 'Sam'));
 
 /*
 Display department names
 where the maximum salary
-is greater than Grace’s salary.
+is greater than Sam’s salary.
 */
+select dept_name
+from Departments
+where dept_id in 
+(select dept_id 
+from Employees 
+Group by dept_id
+having max(salary) > (select salary from Employees where emp_name = 'Sam'));
 
 /*
 Display department names
 where the minimum salary
-is less than Grace’s salary.
+is less than Sam’s salary.
 */
+select dept_name
+from Departments
+where dept_id in 
+(select dept_id 
+from Employees 
+Group by dept_id
+having min(salary) < (select salary from Employees where emp_name = 'Sam'));
 
 /*
 Display department names
@@ -152,9 +218,25 @@ where the average salary
 is equal to the average salary
 of the Sales department.
 */
+select dept_name
+from Departments
+where dept_id in
+(select dept_id
+from Employees
+Group by dept_id
+having avg(salary) = 
+(select avg(salary) 
+from Employees 
+where dept_id = 
+(select dept_id 
+from Departments 
+where dept_name = 'Finance')));
 
 /*
 Display employee names
 who belong to a department that exists in the Departments table.
 */
-
+select emp_name
+from Employees
+inner join Departments
+On Employees.dept_id = Departments.dept_id;
